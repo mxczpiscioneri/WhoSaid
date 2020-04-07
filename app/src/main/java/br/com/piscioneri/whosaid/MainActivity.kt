24b07b0946a19abf1ed6e90da535baa026f3195d
Matheus Piscioneri <matheus.piscioneri@luizalabs.com>
2020-04-06 22:30:58 -0300

    implement Firebase

--- app/src/main/java/br/com/piscioneri/whosaid/MainActivity.kt
+++ app/src/main/java/br/com/piscioneri/whosaid/MainActivity.kt
@@ -1,19 +1,25 @@
 package br.com.piscioneri.whosaid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        FirebaseAnalytics.getInstance(this);
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container,
-                        MainFragment()
-                    )
-                    .commitNow()
+                .replace(
+                    R.id.container,
+                    MainFragment()
+                )
+                .commitNow()
         }
     }
 }
